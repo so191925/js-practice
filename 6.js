daea888770de54165d02afef9acbe3513f823d7f
so191925 <so191925@gmail.com>
2022-06-29 23:29:05 +0900

    22.06.29

---
+++ 6.js
@@ -0,0 +1,11 @@
+//6. false
+//다음중 false로 취급되지 않는것을 고르시오.
+
+1)  NaN
+2)  1
+3)  ""
+4)  0
+5)  undefined
+
+//2번. 자바스크립트에서의 무조건적인 false값은 undifinde, null, 빈문자열, 0, NaN, false이다.
+//이를 제외한 모든 값은 true이다.
